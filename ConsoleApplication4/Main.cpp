b12e3141597a82f7712b2019ca7d3d6f5037a6ce
MCanterel <nelsnelsnels@gmail.com>
2017-05-24 22:01:05 -0400

    Consolidated stepping through folder w proper unicode calls.

---
+++ ConsoleApplication4/Main.cpp
@@ -0,0 +1,48 @@
+// ConsoleApplication4.cpp : Defines the entry point for the console application.
+
+#include "stdafx.h"
+
+vector<string> bookNums;
+
+int main() {
+	callRoutine();
+	//system("PAUSE");
+}
+
+int oldmain()
+{
+	int input;
+	Menu m;
+
+	do {
+		m.display();
+		input = m.getChoice();
+		if (input == 1) {
+			string bookInput = m.getBookISBN();
+			bookNums.push_back(bookInput);
+			cout << "You inputted: " << bookInput << endl;
+		}
+		if (input == 2) {
+			if (bookNums.size() > 0) {
+
+				for (unsigned int i = 0; i < bookNums.size(); i++) {
+					cout << "Item # " << i + 1 << ": " << bookNums[i] << endl;
+				}
+			}
+		}
+
+		if (input == 3) {
+			//send query and clear bookNums
+			if (sendQuery(bookNums) == 1) {
+				bookNums.clear();
+			}
+			else {
+				cout << "Unable to query web for these listings. Try again." << endl;
+			}
+		}
+
+	} while (input < MENUCOUNT);
+
+	cout << "Goodbye!" << endl;
+	return 0;
+}
